91fcc93e392d14b238f384e4e62bf66f5a059b19
unknown <katlehormakhoba@gmail.com>
2022-02-16 01:49:37 +0200

    auth structure-2

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,28 @@ exports.login = (req, res, next)=> {
         message: 'Welcome to login endpoint'
     })
 }
+
+exports.signup = (req, res, next)=> {
+    res.status(200).json({
+        status: 'success',
+        message: 'Welcome to Signup endpoint'
+    })
+}
+
+
+exports.forgotPassword = (req, res, next)=> {
+    res.status(200).json({
+        status: 'success',
+        message: 'Welcome to Forgot Password endpoint'
+    })
+}
+
+
+exports.updatePassword = (req, res, next)=> {
+    res.status(200).json({
+        status: 'success',
+        message: 'Welcome to Upadate Password endpoint'
+    })
+}
+
+
