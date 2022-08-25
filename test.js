6667a0032cc470890aa278b5d1da5d7f0258c82f
Shiko Matlala <shikomatlala@gmail.com>
2022-08-25 14:59:57 +0200

    Testing from Shiko

--- test.js
+++ test.js
@@ -5,6 +5,5 @@ console.log(idvalidater.validateIdNumber('1191111111111'))
 let t ;
  t = ['Katleho','Jack', 'Mash', 'Thato', 'Shiko']
 const ids = ['Katleho','Jack', 'Mash', 'Thato', 'Shiko'];
-
-
+//Hi there - testing from Shiko
 console.log(ids.join(', '))
